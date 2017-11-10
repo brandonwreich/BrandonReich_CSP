7c13839885c835c80ef995051eb830bfa999f3fe
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-11-10 09:55:16 -0700

    Added in all of my photos with a image switcher adn started to work on their labels

--- BrandonReich_CSP/Controller/GlobalImpactViewController.swift
+++ BrandonReich_CSP/Controller/GlobalImpactViewController.swift
@@ -8,20 +8,73 @@
 
 import UIKit
 
-class GlobalImpactViewController: UIViewController
+public class GlobalImpactViewController: UIViewController
 {
-    @IBOutlet weak var UIImageViewAIDS: UIImageView!
+    private var imageCounter : Int = 0
+    
+    @IBOutlet weak var UIImagePlans: UIImageView!
+    @IBOutlet weak var PersonalConnection: UILabel!
+    @IBOutlet weak var NeedToAddress: UILabel!
+    @IBOutlet weak var DataPlan: UILabel!
+    @IBOutlet weak var pictureButton: UIButton!
+
+    @IBAction func changePicture(_ sender: UIButton)
+    {
+        switchImage()
+    }
+    
+    private func placeImage() -> Void
+    {
+        UIImagePlans.image = UIImage(named: "Plans")
+    }
+    
+    private func switchImage() -> Void
+    {
+        if (imageCounter > 1)
+        {
+            imageCounter = 0
+        }
+        
+        if (imageCounter == 0)
+        {
+            UIImagePlans.image = UIImage(named: "Survey")
+        }
+        else
+        {
+            UIImagePlans.image = UIImage(named: "Plans")
+        }
+        
+        imageCounter+=1
+    }
+    
+    private func fillDataPlanLabel() -> Void
+    {
+        DataPlan.text = ""
+    }
+    
+    private func fillNeedToAddressLabel() -> Void
+    {
+        NeedToAddress.text = ""
+    }
+    
+    private func fillPersonalConnectionLabel() -> Void
+    {
+        PersonalConnection.text = ""
+    }
     
     
-    
-    override func viewDidLoad()
+   public override func viewDidLoad()
     {
         super.viewDidLoad()
+        placeImage()
+        fillDataPlanLabel()
+        fillNeedToAddressLabel()
+        fillPersonalConnectionLabel()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning()
+   public override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
