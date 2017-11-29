8b4e19ce9d9813894fd4c1907ce6555aafbbd911
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-11-29 08:00:02 -0700

    Addidng framework for my Internet page

--- BrandonReich_CSP/Controller/InternetViewController.swift
+++ BrandonReich_CSP/Controller/InternetViewController.swift
@@ -8,22 +8,54 @@
 
 import UIKit
 
-class InternetViewController: UIViewController
+public class InternetViewController: UITableViewController
 {
+    private (set) lazy var internetTopics : [String] =
+    {
+        return [
+        "Definitionis",
+        "CSP",
+        "CTEC",
+        "Canyons",
+        "Twitter",
+        "Swift Guide"
+        ]
+    }()
+    
+    private var detailViewController: InternetDetailViewController?
+    
+    private func setup() -> Void
+    {
+        
+    }
 
-    override func viewDidLoad()
+    override public func viewDidLoad()
     {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setup()
     }
     
+    
+    //MARK: - Table View Data Source
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
