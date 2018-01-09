c5fffe04ae6f6708556d708fdef4898e4795d782
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-01-09 08:21:26 -0700

    aded the bucket list to the code

--- BrandonReich_CSP/Controller/DataView/DataAndInformationViewController.swift
+++ BrandonReich_CSP/Controller/DataView/DataAndInformationViewController.swift
@@ -10,7 +10,56 @@ import UIKit
 
 class DataAndInformationViewController: UITableViewController
 {
-
+    lazy var bucketList : [BucketItem] =
+        {
+            return loadBucketListFromFile()
+    }()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource: "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0], author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File load error")
+            }
+        }
+        
+        return items
+    }
+    
+    //MARK: Table View Code
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+    return 1
+    }
+    
+    override public func tableView(_ tabelView: UITableView, numberOfRowsInSection ssection: Int) -> Int
+    {
+        return bucketList.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
+        
+        currentCell.bucketItem = bucketList[indexPath.row]
+        
+        return currentCell
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
